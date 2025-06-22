cd6b6bd51ca65096a7f4716025a47d6b8922d690
Marco Márquez <marco.marquez@gpwrkng.local>
2025-06-21 20:47:24 -0600

    tercer comit

---
+++ resources/views/productos/index.blade.php
@@ -0,0 +1,12 @@
+@extends("plantillas.plantilla")
+
+@section("title","productos")
+
+@section("content")
+<h1>Bienvenidos a la seccion de Productos</h1>
+@endsection
+
+@section("seccion")
+<h2>Hola mundo</h2>
+
+@endsection
---
+++ resources/views/productos/show.blade.php
@@ -0,0 +1,9 @@
+@extends("plantillas.plantilla")
+
+@section("title","producto")
+
+@section("content")
+<h1> Tu producto es : <?php echo $prod?</h1>
+@endsection
+
+
