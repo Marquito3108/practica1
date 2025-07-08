5452f88cd04b576b08b567a942b29267536f1f36
Marco Márquez <marco.marquez@gpwrkng.local>
2025-07-08 13:48:58 -0600

    08/07/2025

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\indexcontroller;
-use App\Http\Controllers\productoscontroller;
-
-Route::get('/', indexcontroller::class);
+//use App\Http\Controllers\indexcontroller;
+//use App\Http\Controllers\productoscontroller;
 
 
 
-Route::controller(productoscontroller::class)->group(function(){
-    Route::get('productos',"index");
 
-    Route::get('productos/crear',"create");
-
-    Route::get('productos/{product}',"show");
-
-
+Route::get('/', function () {
+    return "bienvenido a la pagina principal";
+    
 });
+
+Route::get('/posts', function(){
+    return "Aqui se mostraran todos los posts";
+});
+
+
+Route::get('/posts/{post}', function($post){
+    return "Aqui se mostrara el post {$post}";
+});
+
+
+
+
+//Route::get('/', indexcontroller::class);
+
+
+
+//Route::controller(productoscontroller::class)->group(function(){
+ //   Route::get('productos',"index");
+
+   // Route::get('productos/crear',"create");
+
+ //   Route::get('productos/{product}',"show");
+
+
+//});
